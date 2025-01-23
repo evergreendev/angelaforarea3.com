307ea79d914108a9783cfa7119b9245dc45b96c5
Joe simpson <joe@egmrc.com>
2025-01-23 14:26:20 -0700

    Update google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${glacialIndifference.variable} ${sportsWorld.variable}`}>
-    <GoogleAnalytics gaId="G-8GQ8EYPGLH"/>
+    <GoogleAnalytics gaId="G-6PECGC3YP8"/>
       <body>
       <Nav/>
       {children}
